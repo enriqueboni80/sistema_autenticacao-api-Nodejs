e47ff69796304edf21b981298d99ca8c230ebc37
rickboni80 <enriqueboni80@gmail.com>
2020-07-08 21:11:40 -0300

    Consertando bug do Logout!

--- middlewares/ensureAuthenticated.js
+++ middlewares/ensureAuthenticated.js
@@ -5,35 +5,36 @@ var jwtDisableList = require("../jwt-disablelist")
 exports.ensureAuthenticated = (roles = null) => {
     return function (req, res, next) {
         const jwtToken = req.headers.authorization.split(' ')[1]
-        
-        if (jwtDisableList.list.length > 0) {
-            console.log(jwtDisableList.list.forEach((jwtTokenDeny) => {
-                if(jwtToken === jwtTokenDeny){
-                    return res.status(401).send('TOKEN DESABILITADO')
-                }
-            }))
-        }
 
-        try {
-            const decode = jwt.verify(jwtToken, process.env.JWT_KEY)
-            req.usuario = decode;
-            req.usuario.jwtToken = jwtToken
-            var grupos = decode.grupos
-            var podeContinuar = false
-            if (!roles) {
-                podeContinuar = true
-            } else {
-                grupos.forEach(grupo => {
-                    roles.map((role) => {
-                        if (grupo === role) {
-                            podeContinuar = true
-                        }
-                    })
-                });
+        if (jwtDisableList.list.length > 0) {
+            jwtDisableList.list.forEach((jwtTokenDeny) => {
+                if (jwtToken === jwtTokenDeny) {
+                    res.status(401).send('TOKEN DESABILITADO')
+                }
+            })
+        }
+        else {
+            try {
+                const decode = jwt.verify(jwtToken, process.env.JWT_KEY)
+                req.usuario = decode;
+                req.usuario.jwtToken = jwtToken
+                var grupos = decode.grupos
+                var podeContinuar = false
+                if (!roles) {
+                    podeContinuar = true
+                } else {
+                    grupos.forEach(grupo => {
+                        roles.map((role) => {
+                            if (grupo === role) {
+                                podeContinuar = true
+                            }
+                        })
+                    });
+                }
+                podeContinuar ? next() : res.status(401).send('SEM PERMISSÃO PARA ESSA ROTA')
+            } catch (error) {
+                return res.status(401).send('FALHA NA VERIFICACAO')
             }
-            podeContinuar ? next() : res.status(401).send('SEM PERMISSÃO PARA ESSA ROTA')
-        } catch (error) {
-            return res.status(401).send('FALHA NA VERIFICACAO')
         }
     }
 }
