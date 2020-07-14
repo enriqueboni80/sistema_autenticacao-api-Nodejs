b1000b451098da1d678477fb3b543d689f557dc1
rickboni80 <enriqueboni80@gmail.com>
2020-07-14 17:25:24 -0300

    Migrate: setando email como unique

--- migrations/20200130234742_create_users.js
+++ migrations/20200130234742_create_users.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('users', table => {
         table.increments('id').unsigned().primary()
         table.string('nome').notNull()
-        table.string('email').notNull()
+        table.string('email').notNull().unique()
         table.string('password').notNull()
         table.string('activation_token').nullable()
         table.boolean('active').defaultTo(false)
