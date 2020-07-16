d86c04bb0793c6faaf291d56f4718118819bb00f
rickboni80 <enriqueboni80@gmail.com>
2020-07-15 21:28:40 -0300

    Rafatorando LoginController

--- Controllers/Auth/LoginController.js
+++ Controllers/Auth/LoginController.js
@@ -5,44 +5,42 @@ var jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 module.exports = {
-    autenticar(req, res, next) {
-        var email = req.body.email
-        var password = req.body.password
-        User.getByEmail(email).then((user) => {
+    async autenticar(req, res) {
+        try {
+            let user = await User.getByEmail(req.body.email)
             if (!user) {
                 return res.status(401).send('FALHA NA AUTENTICACAO')
-            } else {
-                var passwordsSaoIguais = User.compararPasswordsBycrypt(password, user.password)
-                if (passwordsSaoIguais) {
-                    if (!user.active) {
-                        return res.status(401).send('USUARIO NÃO ATIVADO')
-                    } else {
-                        //ele checar aqui em quais grupos o usuario pertence - Necessário refatorar
-                        var _gruposQuePertence = []
-                        gruposUsuarios.getGroupsById(user.id).then((result) => {
-                            result.map((group) => {
-                                _gruposQuePertence.push(group.grupo_id)
-                            })
-                            user.grupos = _gruposQuePertence
-                            let _jwtToken = jwt.sign({
-                                id: user.id,
-                                email: user.email,
-                                grupos: user.grupos
-                            }, process.env.JWT_KEY, {
-                                expiresIn: "9h"
-                            })
-                            return res.status(200).send({
-                                success: true,
-                                mensagem: "AUTENTICADO COM SUCESSO",
-                                jwtToken: _jwtToken
-                            })
-                        })
-                    }
-                }
-                else {
-                    return res.status(401).send('FALHA NA AUTENTICACAO')
+            }
+            var passwordsSaoIguais = User.compararPasswordsBycrypt(req.body.password, user.password)
+            if (passwordsSaoIguais) {
+                if (!user.active) {
+                    return res.status(401).send('USUARIO NÃO ATIVADO')
+                } else {
+                    let gruposUsuario = await gruposUsuarios.getGroupsById(user.id)
+                    let gruposQuePertence = []
+                    gruposUsuario.map((result) => {
+                        gruposQuePertence.push(result.grupo_id)
+                    })
+                    user.grupos = gruposQuePertence
+                    let _jwtToken = jwt.sign({
+                        id: user.id,
+                        email: user.email,
+                        grupos: user.grupos
+                    }, process.env.JWT_KEY, {
+                        expiresIn: "9h"
+                    })
+                    return res.status(200).send({
+                        success: true,
+                        mensagem: "AUTENTICADO COM SUCESSO",
+                        jwtToken: _jwtToken
+                    })
                 }
             }
-        })
+            else {
+                return res.status(401).send('FALHA NA AUTENTICACAO')
+            }
+        } catch (error) {
+            return res.status(400).json({ error: error.message })
+        }
     }
 }
