619cbeb087b02aa2dcbeadbec9e54f1961b545ca
rickboni80 <enriqueboni80@gmail.com>
2020-10-26 22:35:49 -0300

    Configurando o App para utilizar o postgres do heroku

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,19 @@ require('dotenv').config()
 
 module.exports = {
 
-    development: {
-        client: 'mysql',
-        version: '5.5',
-        connection: {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
-        },
-        useNullAsDefault: true
-    },
+    /*     development: {
+            client: 'mysql',
+            version: '5.5',
+            connection: {
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_DATABASE
+            },
+            useNullAsDefault: true
+        }, */
 
-    test: {
+    development: {
         client: 'sqlite3',
         connection: {
             filename: './sqlite-test.db'
@@ -29,12 +29,29 @@ module.exports = {
         }
     },
 
+    test: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        useNullAsDefault: true,
+        migrations: {
+            tableName: "knex_migrations",
+            directory: './migrations'
+        },
+        seeds: {
+            directory: './seeds'
+        },
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
+
     staging: {
-        client: 'postgresql',
+        client: 'pg',
         connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
         },
         pool: {
             min: 2,
