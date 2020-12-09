53302c9fd848659e99c27a08b2cdc948bb3ecae0
Enrique Bonifacio <enriqueboni80@gmail.com>
2020-12-08 22:41:54 -0300

    retirando console.logs sem necessidade

--- controllers/EventosInscricoesController.js
+++ controllers/EventosInscricoesController.js
@@ -3,7 +3,6 @@ const EventosInscricoes = require('../service/EventosInscricoes');
 
 module.exports = {
     async index(req, res, next) {
-        console.log(req.body);
         try {
             await EventosInscricoes.inscrever(req.body.evento_id, req.body.user_id)
             return res.status(201).json({ success: true, message: 'ok' });
