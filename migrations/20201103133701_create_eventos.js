c734023a9aaa0127ce34758807f7adcced7294a9
Enrique Bonifacio <enriqueboni80@gmail.com>
2020-12-14 22:14:53 -0300

    Converte migrate evento de dateTime para timestamp

--- migrations/20201103133701_create_eventos.js
+++ migrations/20201103133701_create_eventos.js
@@ -14,9 +14,9 @@ exports.up = function(knex) {
         table.boolean('gratuito').defaultTo(true).notNull()
         table.boolean('privado').defaultTo(false).notNull()
         table.boolean('cancelado').defaultTo(false).notNull()
-        table.dateTime('data_inicio').notNull()
-        table.dateTime('data_fim').notNull()
-        table.dateTime('prazo_inscricao').nullable()
+        table.timestamp('data_inicio').notNull()
+        table.timestamp('data_fim').notNull()
+        table.timestamp('prazo_inscricao').nullable()
         table.timestamps(true, true)
     })
 };
