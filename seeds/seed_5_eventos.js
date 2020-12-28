f0d3b3feb79c56244989b32c240c75354562abeb
Enrique Bonifacio <enriqueboni80@gmail.com>
2020-12-28 16:38:46 -0300

    aumentando campo descricao e criando descricao seed eventos

--- seeds/seed_5_eventos.js
+++ seeds/seed_5_eventos.js
@@ -4,7 +4,19 @@ var evento1 = {
   "palestrante": "Dale Carnegie",
   "url_imagem": "https://www.selecoes.com.br/wp-content/uploads/2019/05/happy-women-hugging-each-other-picture-id929930552-760x450.jpg",
   "detalhes": "Evento voltado para o publico da Pucminas",
-  "descricao": "",
+  "descricao": `<p><span style="font-size: 1.5em; font-weight: 700;">O que você irá aprender</span><br>
+  </p><p>Princípios de &quot;Como fazer amigos e influenciar pessoas&quot; mostram a você como influenciar os outros para apoiarem sua visão. 
+  Descubra 9 técnicas para aprimorar os relacionamentos e técnicas fundamentais para tornar-se um líder respeitado. Dicas para lidar com as críticas 
+  e o estresse liberam você para ir adiante com novas ideias e parcerias mais fortes.</p><p><br>
+  </p><p><span style="font-size: 1.5em; font-weight: 700;">Porque você precisa disso</span><br>
+  <p>Por mais de um século, a Dale Carnegie vem aprimorando o desempenho individual e empresarial pelo mundo. Enquanto nossas técnicas continuam evoluindo, 
+  nossos princípios básicos permanecem leais a uma única visão: a transformação real começa de dentro. Da convicção de um homem no poder de transformar indivíduos, 
+  a Dale Carnegie continua a inspirar a excelência individual ao lidar com o potencial de cada um.</p><p><br>
+  </p><p><span style="font-size: 1.5em; font-weight: 700;">O que você vai fazer com isso</span><br>
+  <p>Conforme emprega os princípios de Dale Carnegie, você se tornará um líder excelente enquanto conquista a confiança e o 
+  respeito das pessoas ao seu redor. Construa a mudança positiva inspirando uma força de trabalho de excelência e com recursos diversos. 
+  Ao conquistar as pessoas para sua forma de pensar e reduzir o estresse, você vai colocar as equipes em movimento e ajudar organizações 
+  inteiras a atingir novos patamares.</p>`,
   "categoria": 5,
   "preco": 50.00,
   "publicado": true,
@@ -22,7 +34,21 @@ var evento2 = {
   "palestrante": "Ayrton Senna",
   "url_imagem": "https://s.ebiografia.com/img/se/nn/senna_tricampea_o_655_x_425.jpg",
   "detalhes": "Evento para esportistas",
-  "descricao": "",
+  "descricao": `<p>​<span style="color: rgb(51, 51, 51); font-family: &quot;Helvetica Neue&quot;, sans-serif; font-size: 15.21px; font-weight: 700;">  
+  DESCRIÇÃO</span>​<br>
+  </p><p><br></p>
+  <p>Começo de tudo</p>
+  <p><br></p>
+  <p>Em 1982, Senna disputou o Campeonato Europeu 1600. Nesse mesmo ano, transferiu-se para a 
+  Fórmula Fiat 2000. A entrada de Ayrton Senna na Fórmula 1 começou em 1983, quando disputou a Fórmula 3 na Inglaterra. Bem-sucedido nessa temporada, 
+  obteve propostas para competir na McLaren e na Williams. Acabou escolhendo uma equipe pequena, a Toleman.&nbsp;<br><br>
+  Em 1985, Senna passou a competir pela Lotus, uma equipe de tamanho médio com a qual preparou o salto que daria em sua carreira. 
+  Na Lotus, disputou 48 grandes prêmios entre 1985 e 1987, vencendo seis vezes. Passou a competir com pilotos consagrados, como Alain Prost, 
+  Nigel Mansell e Nelson Piquet.​</p>
+  <p><br></p><p>Primeiras vitórias</p>
+  <p><br></p>
+  <p>​1985 Equipe Lotus:<br>21/04, GP de Portugal, circuito do Estoril<br>15/09, GP da Bélgica, circuito de Spa-Francorchamps<br><br>
+  <br></p><p><br></p>`,
   "categoria": 5,
   "preco": 50.00,
   "publicado": true,
@@ -40,7 +66,24 @@ var evento3 = {
   "palestrante": "Professor Xavier",
   "url_imagem": "https://portal.pucminas.br/imagedb/pucinforma/PIM_ARQ_IMAGE_PEQUE20200305144105.jpg",
   "detalhes": "Evento acadêmico",
-  "descricao": "",
+  "descricao": `<p><span style="font-size: 1.17em; font-weight: 700;">DESCRIÇÃO</span><br>
+  </p><p><span style="font-size: 1.17em; font-weight: 700;"><br>
+  </span><span style="color: rgb(153, 153, 153); font-family: Poppins, Arial, sans-serif; font-size: 14px; font-style: normal; 
+  font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: justify; text-indent: 0px; 
+  text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(248, 249, 250); 
+  text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;">
+  Nos dias 23, 24 e 25 de abril de 2019, os cursos e setores da PUC São Gabriel se juntam para oferecer a Semana de Inovação, Tecnologia e Empreendedorismo - SITE 2019. 
+  Esse evento vem convergir ações de interesse dos cursos e setores, oferecendo diversas atividades para as comunidades acadêmica e externa. Podemos citar palestras, 
+  oficinas tecnológicas, feiras de empregos, eventos de empreendedorismo tecnológico, mostras de projetos, interação entre empresas e alunos, entre outras ações. 
+  A Semana de Inovação, Tecnologia e Empreendedorismo (SITE) da PUC Minas São Gabriel tem como objetivo a formação geral da comunidade acadêmica e a integração com a 
+  comunidade externa por meio da difusão e debates de novas tecnologias, formas do saber, do fazer e de como empreender.</span></p>
+  <p><span style="color: rgb(153, 153, 153); font-family: Poppins, Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; 
+  font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: justify; text-indent: 0px; text-transform: none; white-space: normal; 
+  widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(248, 249, 250); text-decoration-thickness: initial; text-decoration-style: 
+  initial; text-decoration-color: initial; display: inline !important; float: none;"><br>
+  </span></p><p><span style="color: rgb(153, 153, 153); font-family: Poppins, Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: justify; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(248, 249, 250); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;">Os temas da Semana de Inovação, Tecnologia e Empreendedorismo (SITE) tem relação com tudo aquilo que transforma padrões já estabelecidos, seja uma ideia, método produtivo ou objeto. Inovações tecnológicas servem para melhorar a competitividade das empresas aumentando o valor agregado, podendo envolver pequenos esforços que propiciam melhoria de produtos ou na eficiência de processos ou, também, pode promover uma melhor racionalização de recursos naturais.</span>​<br>
+  </p><p><br></p><p>​<span style="color: rgb(153, 153, 153); font-family: Poppins, Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: justify; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(248, 249, 250); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;">Quanto a produtos, a inovação consiste em alterações nas suas características ou uso. Já em serviços, pode ocorrer a quebra de paradigmas como aconteceu recentemente com transportes e hospedagem. Com relação a processos, é possível ressaltar mudanças como a manufatura aditiva (impressão 3D) e os novos conceitos introduzidos pela Indústria 4.0 ou quarta revolução industrial. A inovação está presente nas mais diversas áreas do conhecimento como comunicação, marketing, turismo, psicologia, medicina, engenharia, direito, administração, dentre outras.</span>​<br>
+  </p>`,
   "categoria": 5,
   "preco": 50.00,
   "publicado": true,
